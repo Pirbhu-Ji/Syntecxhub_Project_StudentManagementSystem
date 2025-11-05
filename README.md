a953a3022d80315f361ab99b2f05eeabab76ce71
Pirbhu-Ji <pirbhuji.becsef24@iba-suk.edu.pk>
2025-11-05 22:47:55 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,45 @@
+#Syntecxhub_Project_StudentManagementSystem 
+
+##Overview
+This project is developed as part of my **Syntecxhub Java Internship (Week 2 Task)**.
+
+It is a **console-based Student Management System** built using **Core Java**,
+which allows user to manage student academic records easily. The system supports adding, 
+displaying, searching, updating, and deleting student records. It also includes file 
+handling so that records are saved permanently.
+
+##Features
+✅ Add new student record  
+✅ Display all student records  
+✅ Search student by ID  
+✅ Update existing student details  
+✅ Delete student record  
+✅ Prevent duplicate Student IDs  
+✅ Validate marks (0 – 100 range)  
+✅ Automatically save records to file  
+✅ Load records on program start  
+✅ Fully modular OOP-based system  
+
+##Technologies Used
+- Java (OOP + Collections + File Handling)
+- ArrayList for data storage
+- Scanner for input handling
+- Any Java-supported IDE (VS Code / IntelliJ / Eclipse)
+- GitHub for version control
+
+## Project Structure
+src/
+  ├── Student.java // Student model class
+  ├── StudentManager.java // Handles all operations (CRUD + validation + file save)
+  └── StudentManagementSystem.java // Main program menu
+students.txt // Data storage file
+
+## Author
+**Pirbhuji**  
+Java Intern @Syntecxhub  
+GitHub: https://github.com/Pirbhu-Ji  
+
+## Project Status
+This project has been **successfully completed and submitted** as part of 
+**Syntecxhub Internship Week-2 Task**.
+
---
+++ StudentManagementSystem.java
@@ -0,0 +1,200 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student{
+   public int stdID;
+   public String name;
+   public int marks;
+
+    public Student(int stdID, String name, int marks) {
+        this.stdID=stdID;
+        this.name=name;
+        this.marks=marks;
+    }
+
+  public String toString(){
+      return "Student-> ID: "+stdID+", Name: "+name+", Marks: "+marks;
+     }  
+}
+
+class StudentManager{
+   private ArrayList<Student> students=new ArrayList<>();
+   Scanner in=new Scanner(System.in);
+
+   void addStudent(){
+        System.out.print("Enter Student ID: ");
+        int id = in.nextInt();
+        in.nextLine(); // newline left by 
+
+        System.out.print("Enter Student Name: ");
+        String name = in.nextLine();
+        System.out.print("Enter Marks: ");
+        int marks = in.nextInt();
+
+        students.add(new Student(id, name, marks));
+        System.out.println("Student added successfully!\n");
+    }
+
+   void displayStudents(){
+      int size=students.size();
+      if(size>0){
+         System.out.println("=== Student List ===");
+          for (int i = 0; i < size; i++) {
+          System.out.println(students.get(i).toString());
+          }
+         System.out.println();
+        }
+      else{
+        System.out.println("No Student Record yet!");
+         }
+    }
+      
+    void searchStudent(){
+        if (students.isEmpty()){
+            System.out.println("No student records to search.\n");
+            return;
+        }
+
+        System.out.print("Enter Student ID to search: ");
+        int search = in.nextInt();
+        boolean found = false;
+
+        for (Student s : students) {
+            if (s.stdID == search) {
+                System.out.println("Record found: " + s.toString());
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Student not found!\n");
+        }
+    }
+    
+       void deleteStudent(){
+        if (students.isEmpty()){
+            System.out.println("No student records to delete.\n");
+            return;
+        }
+
+        System.out.print("Enter Student ID to remove: ");
+        int remove = in.nextInt();
+        boolean found = false;
+
+        for (int i = 0; i < students.size(); i++){
+            if (students.get(i).stdID == remove){
+                System.out.println("Removing: " + students.get(i).toString());
+                students.remove(i);
+                System.out.println("Removed successfully!\n");
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Student not found!\n");
+        }
+    
+    }
+
+    void updateStudent(){
+         if (students.isEmpty()){
+        System.out.println("No student records to update!\n");
+        return;
+         }
+
+       System.out.print("Enter Student ID to update: ");
+       int id = in.nextInt();
+       //in.nextLine(); // consume newline
+       boolean found = false;
+
+      for (Student s : students){
+        if (s.stdID == id) {
+            System.out.println("Current record: " + s.toString());
+             in.nextLine();
+            System.out.print("Enter new name: ");
+            s.name = in.nextLine();
+            System.out.print("Enter new marks: ");
+            s.marks = in.nextInt();
+            System.out.println("Record updated successfully!\n");
+            found = true;
+            break;
+        }
+     }
+
+      if (!found) {
+        System.out.println("Student not found!\n");
+       }
+     }
+
+     void saveToFile(){
+           try{
+                 FileWriter writer = new FileWriter("students.txt"); // overwrite
+             for (Student s : students) {
+               writer.write(s.stdID + "," + s.name + "," + s.marks + "\n");
+             }
+             writer.close();
+             System.out.println("Student records saved to students.txt");
+            } catch (IOException e){
+              System.out.println("Error saving data: " + e.getMessage());
+            }
+         }
+     void loadFromFile(){
+          try{
+               FileReader fr = new FileReader("students.txt");
+             BufferedReader br = new BufferedReader(fr);
+               String line;
+
+         while((line = br.readLine()) != null){
+            String[] data = line.split(",");
+            int id = Integer.parseInt(data[0]);
+            String name = data[1];
+            int marks = Integer.parseInt(data[2]);
+            students.add(new Student(id, name, marks));
+         }
+             br.close();
+           System.out.println("Previous data loaded successfully.\n");
+         } catch (IOException e) {
+              System.out.println("No saved data found, starting fresh.\n");
+           }
+       }
+  
+}
+
+public class StudentManagementSystem {
+    public static void main(String[] args) {
+         Scanner cin=new Scanner(System.in);
+         StudentManager std = new StudentManager();
+         std.loadFromFile(); // load stored data
+           int choice;
+
+         do {
+            System.out.println("===== Student Management System =====");
+            System.out.println("1. Add Student");
+            System.out.println("2. Display All Students");
+            System.out.println("3. Search Student");
+            System.out.println("4. Update Student");
+            System.out.println("5. Delete Student");
+            System.out.println("6. Save & Exit");
+            System.out.print("Enter your choice: ");
+            choice = cin.nextInt();
+
+            switch(choice){
+                case 1: std.addStudent(); break;
+                case 2: std.displayStudents(); break;
+                case 3: std.searchStudent(); break;
+                case 4: std.updateStudent(); break;
+                case 5: std.deleteStudent(); break;
+                case 6: std.saveToFile();
+                         System.out.println("Exiting... Thank you!");break;
+                default: System.out.println("Invalid choice! Try again.\n");
+            }
+        } while (choice != 6);
+        
+    }
+}
